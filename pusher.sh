5fa40dcf9cb5344f421e4ff36d6f0dd1081c1865
javier20dev25 <sleytherjavier2025@gmail.com>
2025-09-16 17:54:22 -0600

    Primer commit desde Termux

---
+++ pusher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Configura la URL remota con un placeholder para el token
+git remote set-url origin https://javier20dev25:PEGA_AQUI_TU_NUEVO_TOKEN@github.com/javier20dev25/Pacifico_Web.git
+
+# Sube el código a GitHub
+git push -u origin main
